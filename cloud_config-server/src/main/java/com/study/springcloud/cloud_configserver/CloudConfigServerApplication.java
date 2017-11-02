dc201839d5941d1cf562b837cda2c6a3c964290f
‘e <710368779@qq.com>
2017-11-02 20:34:09 +0800

    first

--- cloud_config-server/src/main/java/com/study/springcloud/cloud_configserver/CloudConfigServerApplication.java
+++ cloud_config-server/src/main/java/com/study/springcloud/cloud_configserver/CloudConfigServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class CloudConfigServerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigServerApplication.class, args);
 	}
